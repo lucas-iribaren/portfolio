b69f1136d1075fc366be3f0ae0f674cb94c59450
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-08-23 15:52:42 +0200

    chore: initialise le projet avec la structure de base
    Ajout des premiers fichiers et de la structure de base du projet. Cela inclut :
    
    - index.php
    - les includes enfant de l'index

---
+++ include/_head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Lucas Iribaren</title>
+    <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<link rel="icon" type="image/png" href="asset\picture\??.png"> -->
+    <!-- css bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css?t=<?= time();?>" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- css perso -->
+    <link rel="stylesheet" href="asset/css/style.css?t=<?= time();?>">
+</head>
