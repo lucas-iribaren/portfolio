08aaef756bf32d868994e0b25475a036ab3b04e6
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-08-25 00:44:57 +0200

    feat(index): ajouter le fichier de base index.php avec la structure HTML
    Ajout du fichier `index.php` incluant la structure HTML de base du site. Ce fichier :
    
    - Contient les inclusions pour les parties communes du site : en-tête, navigation, sections principales (hero, about, projects, skills, contact) et pied de page.
    - Utilise des fichiers inclus pour chaque section, facilitant ainsi la gestion du contenu.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include('include/_head.php'); ?>
+<body>
+    <?php include('include/_header.php'); ?>
+    <?php include('include/_nav.php'); ?>
+    <main>
+        <?php include('include/_hero.php'); ?>
+        <?php include('include/_about.php'); ?>
+        <?php include('include/_projects.php'); ?>
+        <?php include('include/_skills.php'); ?>
+        <?php include('include/_contact.php'); ?>
+    </main>
+    <?php include('include/_footer.php'); ?>
+</body>
+</html>
